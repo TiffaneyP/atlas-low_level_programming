a3f2ef70f58d033b590ad702c400c4925136f098
TiffaneyP <Tiffaney.Phelps@atlasschool.com>
2024-01-18 19:32:55 +0000

    printf

---
+++ hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main -use printf to print an exact string, you jerk.
+ *
+ * Return: always return 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
