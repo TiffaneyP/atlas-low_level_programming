c7a4433a8a9a2b131b7f06707ab0481ee26c6dc4
TiffaneyP <Tiffaney.Phelps@atlasschool.com>
2024-01-19 17:52:36 +0000

    digit

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -1,28 +1,28 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/** betty style doc for function main goes there 
-*main - trying to parse info about last digit in number
+/** 
+*main - displays whether last digit is greater or less than.
+*Return: always return 0
 */
 int main(void)
 {
         int n;
-
         srand(time(0));
         n = rand() - RAND_MAX / 2;
-
-	printf("Last digit of %d is %d", n);
+	printf("Last digit of %d is %d", n, n % 10);
         if (n % 10 > 5)
         {
-            	printf("and is greater than 5");
+            	printf(" and is greater than 5");
         }
         else if (n % 10 == 0)
         {
-            	printf("and is 0");
+            	printf(" and is 0");
         }
         else
         {
-        	printf("and is less than 6 and not 0");
+        	printf(" and is less than 6 and not 0");
         }
+
         return (0);
 }
