dbdcd437433b6a34c7a6a8bd825cd47d3aff5e37
TiffaneyP <Tiffaney.Phelps@atlasschool.com>
2024-01-19 17:56:14 +0000

    alphas

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+*main - displays whether random number is pos, neg, or equal
+*Return: always return 0
+*/
+
+int main(void)
+{
+	char c = 'a';
+	char d = 'A';
+
+	while (c <= 'z')
+	{
+		putchar(c);
+		c++;
+	}
+	while (d <= 'Z')
+	{
+		putchar(d);
+		d++;
+	}
+	putchar('\n');
+	return (0);
+}
