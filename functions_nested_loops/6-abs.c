647d76f028111bef13aefdc87d167b7a551b0fa1
TiffaneyP <Tiffaney.Phelps@atlasschool.com>
2024-01-22 18:53:16 +0000

    abs

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _abs - computes the abs val of an int.
+ *@r: passed in integer
+ *Return: Returned 0
+ */
+int _abs(int r)
+{
+	if (r < 0)
+	{
+		return (-r);
+	}
+	else
+	{
+		return (r);
+	}
+return (0);
+}
