4f5dd97e8649388dd73ebcf222d45185d71c15a4
TiffaneyP <Tiffaney.Phelps@atlasschool.com>
2024-01-22 17:17:21 +0000

    alpa

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_alphabet - print alphabet using _putchar prototype.
+ */
+void print_alphabet(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		_putchar(c);
+		c++;
+	}
+	_putchar('\n');
+}
