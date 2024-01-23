afe8a8c5cc67061617872455c1e9524154deb3ec
TiffaneyP <Tiffaney.Phelps@atlasschool.com>
2024-01-23 00:34:56 +0000

    print

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *print_to_98 - prints form 0 to 98
+ *@n: initial variable
+ *Return: Always 0
+ */
+void print_to_98(int n)
+{
+	int i;
+
+	if (n >= 98)
+	{
+		for (i = n; i >= 98; i--)
+			if (i != 98)
+				printf("%d, ", i);
+			else
+				printf("%d", i);
+	}
+	else
+	{
+		for (i = n; i <= 98; i++)
+			if (i != 98)
+				printf("%d, ", i);
+			else
+				printf("%d", i);
+	}
+	printf("\n");
+}
