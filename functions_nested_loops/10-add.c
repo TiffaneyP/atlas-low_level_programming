3f44487e7e0bc4e6c225049591669f3b2829e44e
TiffaneyP <Tiffaney.Phelps@atlasschool.com>
2024-01-23 00:19:25 +0000

    add

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ *add - adds two numbers and returns result
+ *@a: integer submitted
+ *@b: integer submitted
+ * Return: Always 0
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
