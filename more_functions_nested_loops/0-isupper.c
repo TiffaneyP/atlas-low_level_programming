cad1176f7e01e518113367a0e5c5745e8c785881
TiffaneyP <Tiffaney.Phelps@atlasschool.com>
2024-01-24 18:20:07 +0000

    isupper

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _isupper- Checks to see if something is an uppercase char
+ *@c: int input
+ * Return: 1 if uppercase, 0 if otherwise.
+ */
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	return (0);
+}
