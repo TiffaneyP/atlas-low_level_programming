78fad1cd660d9c833a60a6c7714a6d00760a2b61
TiffaneyP <Tiffaney.Phelps@atlasschool.com>
2024-01-25 17:16:21 +0000

    main

--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -1,12 +1,16 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-int _isupper(int c);
-int _isdigit(int c);
-int mul(int a, int b);
+int _isupper(int);
+int _isdigit(int);
+int mul(int,int);
 void print_numbers(void);
-int _putchar(int c);
+int _putchar(int);
 void print_most_numbers(void);
 void more_numbers(void);
+void print_line(int);
+void print_diagonal(int);
+void print_square(int);
+void Fizz_Buzz(void);
 
 #endif
