27be182d7051c0f5db5fe539b2a4aa11ad44c227
TiffaneyP <Tiffaney.Phelps@atlasschool.com>
2024-01-29 22:11:31 +0000

    rev

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -1,10 +1,26 @@
 #include "main.h"
 /**
- * print_rev- prints a string in reverse
+ * rev_string- reverses a string
  * @s: passed in character as pointer
  *
  * Return: return is void
  */
-void print_rev(char *s)
+void rev_string(char *s)
 {
-	int a, length;
+	int a = 0, length = 0;
+	char swap;
+
+	while (s[length] != '\0')
+	{
+		length++;
+	}
+
+	length--;
+
+	for (; a <= length; a++, length--)
+	{
+		swap = s[length];
+		s[length] = s[a];
+		s[a] = swap;
+	}
+}
