364d188805dc356fb1dc6b018abe3c64bc88283b
TiffaneyP <Tiffaney.Phelps@atlasschool.com>
2024-01-29 19:32:23 +0000

    rev

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -1,9 +1,10 @@
 #include "main.h"
 /**
- * rev_string- reverse a string
- * @s: a string
+ * print_rev- prints a string in reverse
+ * @s: passed in character as pointer
  *
- *Return: returns void
+ * Return: return is void
  */
-void rev_string(char *s)
+void print_rev(char *s)
 {
+	int a, length;
