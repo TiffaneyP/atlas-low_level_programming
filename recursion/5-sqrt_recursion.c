377958d6f53392350b56cb2a5936c7c3f96d7337
TiffaneyP <Tiffaney.Phelps@atlasschool.com>
2024-02-13 04:54:57 +0000

    sqrt

---
+++ recursion/5-sqrt_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _sqrt_return natural square root of number
+ * @n: input number
+ *
+ * Return: square root
+ */
+int sqrt_a(int a, int b)
+{
+	if (b * b == a)
+	{
+		return (b)
+	}
+}
