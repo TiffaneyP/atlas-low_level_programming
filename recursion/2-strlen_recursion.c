95c2a4c81e39695d4af15b184a7c3a7cc1d1e0b7
TiffaneyP <Tiffaney.Phelps@atlasschool.com>
2024-02-13 03:24:59 +0000

    length

---
+++ recursion/2-strlen_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _strlen_recursion - print length of string
+ *
+ *  @s: string
+ *
+ *  Return: length of string as int
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return (0);
+	}
+	else
+	{
+		return (1 + _strlen_recursion(s + 1));
+	}
+}
