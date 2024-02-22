573a76be7b0ef1e00064bfa1b035a828d671bd53
TiffaneyP <Tiffaney.Phelps@atlasschool.com>
2024-02-22 09:41:28 +0000

    argc

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - prints name
+ * @argc: arg count
+ * @argv: arg vector
+ *
+ * Return: zero
+ */
+int main(int argc, char *argv[])
+{
+	(void) argc;
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
