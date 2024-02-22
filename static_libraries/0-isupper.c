efae269b09b820b5c17e1d533aa783b8666b0a55
TiffaneyP <Tiffaney.Phelps@atlasschool.com>
2024-02-22 10:21:16 +0000

    check

--- static_libraries/0-isupper.c
+++ static_libraries/0-isupper.c
@@ -1,1 +1,17 @@
 #include "main.h"
+/**
+ * _isupper- check for uppercase                       *@c: int input
+ * Return: 1 if uppercase, 0 if otherwise.
+ */
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	return (0);
+}
