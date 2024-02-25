184f83228127e2a94eb03c9a481f91f059864928
TiffaneyP <Tiffaney.Phelps@atlasschool.com>
2024-02-25 11:43:05 +0000

    function

---
+++ function_pointers/3-main.c
@@ -0,0 +1,31 @@
+#include "3-calc.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - function for argc and argv 
+ * @argc: count
+ * @argv: vector
+ * Return: return an int
+ */
+int main(int argc, char *argv[])
+{
+	int (*oprt)(int, int);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	oprt = get_op_func(argv[2]);
+
+	if (!oprt)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	printf("%d\n", oprt(atori(argv[1]), atoi(argv[3])));
+	return (0);
+}
